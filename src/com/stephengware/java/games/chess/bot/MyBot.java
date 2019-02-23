2028a61cb040f21115ff1d6c8133b1558a6d0958
Md Kauser Ahmmed <mahmmed@uno.edu>
2019-02-23 02:47:29 -0600

    update utility, not complete.

--- src/com/stephengware/java/games/chess/bot/MyBot.java
+++ src/com/stephengware/java/games/chess/bot/MyBot.java
@@ -40,109 +40,27 @@ public class MyBot extends Bot {
 		if(player == null) {
 			player = root.player;
 		}
-//		// This list will hold all the children nodes of the root.
-//		ArrayList<State> children = new ArrayList<>();
-//		// Generate all the children nodes of the root (that is, all the
-//		// possible next states of the game.  Make sure that we do not exceed
-//		// the number of GameTree nodes that we are allowed to generate.
-//				
-//		Iterator<State> iterator = root.next().iterator();
-//		
-//		while(!root.searchLimitReached() && iterator.hasNext())
-//			children.add(iterator.next());
-//		// Choose one of the children at random.
-//		
-//		return children.get(random.nextInt(children.size()));
-		
-		return minMax(root);
-	}
 	
-	public State minMax(State state) {
-		GameTree root = new GameTree(state);
-		double value;
+		// This list will hold all the children nodes of the root.
+		ArrayList<State> children = new ArrayList<>();
+		// Generate all the children nodes of the root (that is, all the
+		// possible next states of the game.  Make sure that we do not exceed
+		// the number of GameTree nodes that we are allowed to generate.
 				
-		if(state.player.name() == player.name())
-			value = findMax(root);
-		else
-			value = findMin(root);
+		Iterator<State> iterator = root.next().iterator();
+		State bestChild = null; 
 		
-		for(GameTree child : root.children)
-			if(child.value == value)
-				return child.state;
-		return null;
-	}
-	
-	/**
-	 * Given a {@link GameTree} node, expand its children (if any) to find the
-	 * node with the highest minimum utility value.
-	 * 
-	 * @param tree the node whose children need to be expanded
-	 * @return the utility value of the node with the highest minimum utility
-	 */
-	private double findMax(GameTree tree) {
-		// First, check if this node is a leaf node (i.e. the game is over)
-		// using Tree#state#isTerminal().  If so, simply return the utility of
-		// this state.
-//		if(tree.state.isTerminal()) {
-//			return Utility.evaluate(tree.state);
-//		}
-		
-		if(!tree.hasNextChild()) {
-			return Utility.evaluate(tree.state, player);
-		}
-		
-		// If this node is not a leaf, then we need to expand all of its
-		// children and find the one with the highest minimum utility value.
-		// Start with the lowest possible number, Double#NEGATIVE_INFINITY and
-		// work your way up from there.
-		double max = Double.NEGATIVE_INFINITY;
-		
-		
-		// You can check if a node has more children that have not yet been
-		// explored using GameTree#hasNextChild().
-		while(tree.hasNextChild()) {
-			GameTree child = tree.getNextChild();
-			child.value = findMin(child);
-			max  = Math.max(max, child.value);			
-				
-		}
-		
-		// You can get the next unexplored child node with GameTree#getNextChild().
-		
-		// Find the lowest possible utility value the child node can have.
-		
-		// Update 'max' based on this new information.  'max' should always hold the
-		// largest value we have discovered so far.
-		
-		// Return the highest utility value of all the children nodes.
-		return max;
-	}
-	
-	/**
-	 * Given a {@link GameTree} node, expand its children (if any) to find the
-	 * node with the lowest maximum utility value.
-	 * 
-	 * @param tree the node whose children need to be expanded
-	 * @return the utility value of the node with the lowest maximum utility
-	 */
-	private double findMin(GameTree tree) {
-		// This method is simply the opposite of #findMax.
-//		if(tree.state.isTerminal()) {
-//			return Utility.evaluate(tree.state);
-//		}
-		
-		if(!tree.hasNextChild()) {
-			return Utility.evaluate(tree.state, player);
-		}
-		
-		double min = Double.POSITIVE_INFINITY;
-		
-		while(tree.hasNextChild()) {
-			GameTree child = tree.getNextChild();
-			child.value = findMax(child);
-			min = Math.min(child.value, min);
+		while(!root.searchLimitReached() && iterator.hasNext()) {
+			State child = iterator.next();
 			
+			if(bestChild == null) {
+				bestChild = child;
+			}
+			else if(Utility.evaluate(child, player) > Utility.evaluate(bestChild, player)) {
+				bestChild = child;
+			}
 		}
-		return min;
+		
+		return bestChild;
 	}
 }
--- src/com/stephengware/java/games/chess/bot/Utility.java
+++ src/com/stephengware/java/games/chess/bot/Utility.java
@@ -1,5 +1,15 @@
 package com.stephengware.java.games.chess.bot;
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
+import com.stephengware.java.games.chess.gui.Board;
+import com.stephengware.java.games.chess.state.Bishop;
+import com.stephengware.java.games.chess.state.King;
+import com.stephengware.java.games.chess.state.Knight;
+import com.stephengware.java.games.chess.state.Pawn;
+import com.stephengware.java.games.chess.state.Piece;
 import com.stephengware.java.games.chess.state.Player;
+import com.stephengware.java.games.chess.state.Queen;
+import com.stephengware.java.games.chess.state.Rook;
 import com.stephengware.java.games.chess.state.State;
 
 public class Utility {
@@ -11,11 +21,6 @@ public class Utility {
 	 * @return a positive or negative number or zero
 	 */
 	public static double evaluate(State state, Player player) {
-		if(state.player != player) {
-			System.out.println(state.player.name() + ", " +player.name());
-		}
-		
-		
 		if(state.over) {
 			if(state.check) {
 				if(state.player == player)
@@ -26,20 +31,40 @@ public class Utility {
 			else
 				return 0;
 		}
-		else {
-			int totalPieces = state.board.countPieces();
-			int numPiecesMyBot = state.board.countPieces(player);
-			int numPiecesOtherBot = totalPieces - numPiecesMyBot;
-			
-			System.out.println(numPiecesMyBot + ", " + numPiecesOtherBot);
-			
-			if(numPiecesMyBot > numPiecesOtherBot)
-				return 1;
-			else if (numPiecesMyBot > numPiecesOtherBot) {
-				return -1;
-			}
-			else
-				return 0;
-		}
+		
+		else
+			return materialScore(state, player) - materialScore(state, player.other());
 	}
+	
+	public static double materialScore(State state, Player player) {
+		double score = 0.0;
+		
+		for (Piece piece: state.board) {
+			if(piece.player == player) {
+//				if(piece.getClass() == King.class) {
+//					score += 9.0;
+//				}
+				if(piece.getClass() == Queen.class) {
+					score += 9.0;
+				}
+				if(piece.getClass() == Rook.class) {
+					score += 5.0;
+				}
+				if(piece.getClass() == Bishop.class) {
+					score += 3.0;
+				}
+				if(piece.getClass() == Knight.class) {
+					score += 3.0;
+				}
+				if(piece.getClass() == Pawn.class) {
+					score += 1.0;
+				}
+			}
+			
+		}
+		
+		return score / (9.0 + 2* 5.0 + 2* 3.0 + 8.0);
+	}
+	
+	
 }
